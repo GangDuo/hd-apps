77b70fa78c1926491922a846068761d9c4e528d6
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2019-04-08 14:19:07 +0900

    一番左の列をJANにした在庫表Excelを出力する

--- Logistics.Converter/Stock/Mapping.cs
+++ Logistics.Converter/Stock/Mapping.cs
@@ -10,17 +10,17 @@ namespace Logistics.Converter.Stock
     {
         public Mapping()
         {
-            Map(x => x.SupplierCode).Index(0).Name("仕入先");
-            Map(x => x.SupplierName).Index(1).Name("仕入先名");
-            Map(x => x.VarietyCode).Index(2).Name("部門");
-            Map(x => x.ModelNo).Index(3).Name("商品");
-            Map(x => x.JanCode).Index(4).Name("JAN");
+            Map(x => x.JanCode).Index(0).Name("JAN");
+            Map(x => x.SupplierCode).Index(1).Name("仕入先");
+            Map(x => x.SupplierName).Index(2).Name("仕入先名");
+            Map(x => x.VarietyCode).Index(3).Name("部門");
+            Map(x => x.ModelNo).Index(4).Name("商品");
             Map(x => x.ProductName).Index(5).Name("品名");
             Map(x => x.Price).Index(6).Name("売単価");
             Map(x => x.Qty).Index(7).Name("出荷可能数");
-            Map(x => x.Dmy1).Index(8).Name("次回入荷予定日");
-            Map(x => x.Dmy2).Index(9).Name("入荷予定数");
-            Map(x => x.Dmy3).Index(10).Name("不良在庫");
+            Map(x => x.Dmy2).Index(8).Name("入荷予定数");
+            Map(x => x.Dmy1).Index(9).Name("次回入荷予定日");
+//            Map(x => x.Dmy3).Index(10).Name("不良在庫");
         }
     }
 }
